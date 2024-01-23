5d0cbe20f21243ee96d6d0177fb9e496a1c64ba5
unknown <csmcc2@hotmail.com>
2024-01-23 14:11:41 +0800

    pinyin tone

--- src/tools/pinyin.rs
+++ src/tools/pinyin.rs
@@ -18,6 +18,18 @@ impl Pinyin {
         }
         results
     }
+
+    /// `han_to_pinyin_with_tone` generate 汉字 to hànzì
+    pub fn han_to_pinyin_with_tone(hans: &str) -> String {
+        let mut results = String::new();
+        for pinyin in hans.to_pinyin() {
+            if let Some(pinyin) = pinyin {
+                let result = pinyin.with_tone();
+                results = format!("{}{}", results, result);
+            }
+        }
+        results
+    }
 }
 
 #[test]
@@ -25,4 +37,11 @@ fn han_to_pinyin_test() {
     let result = Pinyin::han_to_pinyin("你好");
     println!("Pinyin: {:?}", result);
     assert_eq!("nihao", result)
+}
+
+#[test]
+fn han_to_pinyin_with_tone_test() {
+    let result = Pinyin::han_to_pinyin_with_tone("你好");
+    println!("Pinyin: {:?}", result);
+    assert_eq!("nǐhǎo", result)
 }
