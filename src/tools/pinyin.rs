9dc0f6ddd4ad173aa2a18de1015f9c9410834ba7
unknown <csmcc2@hotmail.com>
2024-01-31 13:54:08 +0800

    return array

--- src/tools/pinyin.rs
+++ src/tools/pinyin.rs
@@ -7,25 +7,25 @@ pub struct Pinyin {}
 
 #[wasm_bindgen]
 impl Pinyin {
-    /// `han_to_pinyin` generate 汉字 to hanzi
-    pub fn han_to_pinyin(hans: &str) -> String {
-        let mut results = String::new();
+    /// `han_to_pinyin` generate 汉字 to [han,zi]
+    pub fn han_to_pinyin(hans: &str) -> Vec<String> {
+        let mut results: Vec<String> = Vec::new();
         for pinyin in hans.to_pinyin() {
             if let Some(pinyin) = pinyin {
                 let result = pinyin.plain();
-                results = format!("{}{}", results, result);
+                results.push(result.to_string());
             }
         }
         results
     }
 
-    /// `han_to_pinyin_with_tone` generate 汉字 to hànzì
-    pub fn han_to_pinyin_with_tone(hans: &str) -> String {
-        let mut results = String::new();
+    /// `han_to_pinyin_with_tone` generate 汉字 to [hàn,zì]
+    pub fn han_to_pinyin_with_tone(hans: &str) -> Vec<String> {
+        let mut results: Vec<String> = Vec::new();
         for pinyin in hans.to_pinyin() {
             if let Some(pinyin) = pinyin {
                 let result = pinyin.with_tone();
-                results = format!("{}{}", results, result);
+                results.push(result.to_string());
             }
         }
         results
@@ -36,12 +36,12 @@ impl Pinyin {
 fn han_to_pinyin_test() {
     let result = Pinyin::han_to_pinyin("你好");
     println!("Pinyin: {:?}", result);
-    assert_eq!("nihao", result)
+    assert_eq!("nihao", result.join(""))
 }
 
 #[test]
 fn han_to_pinyin_with_tone_test() {
     let result = Pinyin::han_to_pinyin_with_tone("你好");
     println!("Pinyin: {:?}", result);
-    assert_eq!("nǐhǎo", result)
+    assert_eq!("nǐhǎo", result.join(""))
 }
