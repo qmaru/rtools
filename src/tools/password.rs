2719d262cbd297014b8d19471de0b5ee576ff146
unknown <csmcc2@hotmail.com>
2024-07-04 16:14:42 +0800

    return string

--- src/tools/password.rs
+++ src/tools/password.rs
@@ -8,31 +8,35 @@ pub struct Password {}
 
 #[wasm_bindgen]
 impl Password {
-    fn get_random(len: f64, offset: i64) -> i64 {
+    fn get_random(len: f32, offset: i32) -> i32 {
         let mut rng = thread_rng();
-        let base_code: i64 = (rng.gen_range(0f64, 1f64) * len).floor() as i64;
+        let base_code: i32 = (rng.gen_range(0f32, 1f32) * len).floor() as i32;
         let char_code = base_code + offset;
         char_code
     }
 
-    pub fn get_random_upper() -> char {
-        Self::get_random(26f64, 65) as u8 as char
+    pub fn get_random_upper() -> String {
+        let result = Self::get_random(26f32, 65) as u8 as char;
+        String::from(result)
     }
 
-    pub fn get_random_lower() -> char {
-        Self::get_random(26f64, 97) as u8 as char
+    pub fn get_random_lower() -> String {
+        let result = Self::get_random(26f32, 97) as u8 as char;
+        String::from(result)
     }
 
-    pub fn get_random_number() -> char {
-        Self::get_random(10f64, 48) as u8 as char
+    pub fn get_random_number() -> String {
+        let result = Self::get_random(10f32, 48) as u8 as char;
+        String::from(result)
     }
 
-    pub fn get_random_symbol() -> char {
+    pub fn get_random_symbol() -> String {
         let symbol = "!@#$%^&*(){}[]=<>/,.";
-        let len = symbol.len() as f64;
+        let len = symbol.len() as f32;
         let offset = 0;
         let index = Self::get_random(len, offset) as usize;
-        symbol.chars().nth(index).unwrap()
+        let result = symbol.chars().nth(index).unwrap();
+        String::from(result)
     }
 }
 
