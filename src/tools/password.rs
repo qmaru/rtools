e6e4d8a716df4b279f59076548fd557e0db55a06
unknown <csmcc2@hotmail.com>
2024-07-04 16:37:29 +0800

    support byte pass

--- src/tools/password.rs
+++ src/tools/password.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::*;
 
-use rand::{thread_rng, Rng};
+use base64_light::base64_encode_bytes;
+use rand::{thread_rng, Rng, RngCore};
 
 #[wasm_bindgen]
 /// `Password` password tools
@@ -8,6 +9,7 @@ pub struct Password {}
 
 #[wasm_bindgen]
 impl Password {
+    /// `get_random` random char
     fn get_random(len: f32, offset: i32) -> i32 {
         let mut rng = thread_rng();
         let base_code: i32 = (rng.gen_range(0f32..1f32) * len).floor() as i32;
@@ -15,21 +17,25 @@ impl Password {
         char_code
     }
 
+    /// `get_random_upper` randomly generate a upper case letter
     pub fn get_random_upper() -> String {
         let result = Self::get_random(26f32, 65) as u8 as char;
         String::from(result)
     }
 
+    /// `get_random_lower` randomly generate a lower case letters
     pub fn get_random_lower() -> String {
         let result = Self::get_random(26f32, 97) as u8 as char;
         String::from(result)
     }
 
+    /// `get_random_number` randomly generate a number
     pub fn get_random_number() -> String {
         let result = Self::get_random(10f32, 48) as u8 as char;
         String::from(result)
     }
 
+    /// `get_random_symbol` randomly generate a symbol
     pub fn get_random_symbol() -> String {
         let symbol = "!@#$%^&*(){}[]=<>/,.";
         let len = symbol.len() as f32;
@@ -37,6 +43,13 @@ impl Password {
         let index = Self::get_random(len, offset) as usize;
         let result = symbol.chars().nth(index).unwrap();
         String::from(result)
+    }
+
+    /// `get_random_bytes` randomly generate [len] bytes
+    pub fn get_random_bytes(len: usize) -> String {
+        let mut random_bytes_array = vec![0u8; len];
+        rand::thread_rng().fill_bytes(&mut random_bytes_array);
+        base64_encode_bytes(&random_bytes_array)
     }
 }
 
@@ -46,8 +59,9 @@ fn password_random_test() {
     let lower = Password::get_random_lower();
     let number = Password::get_random_number();
     let symbol = Password::get_random_symbol();
+    let bytesp = Password::get_random_bytes(16);
     println!(
-        "upper: {} lower: {} number: {} symbol: {}",
-        upper, lower, number, symbol
+        "upper: {} lower: {} number: {} symbol: {} bytes: {}",
+        upper, lower, number, symbol, bytesp
     );
 }
