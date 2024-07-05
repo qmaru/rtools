f81401ff430183d7d7a37649fff010ee6f994fbc
unknown <csmcc2@hotmail.com>
2024-07-05 15:16:33 +0800

    new method

--- src/tools/password.rs
+++ src/tools/password.rs
@@ -1,67 +1,108 @@
 use wasm_bindgen::prelude::*;
 
 use base64_light::base64_encode_bytes;
-use rand::{thread_rng, Rng, RngCore};
+use rand::{rngs::ThreadRng, seq::SliceRandom, thread_rng, Rng, RngCore};
 
 #[wasm_bindgen]
 /// `Password` password tools
-pub struct Password {}
+pub struct Password {
+    rng: ThreadRng,
+}
 
 #[wasm_bindgen]
 impl Password {
+    pub fn new() -> Self {
+        let rng = thread_rng();
+        Password { rng: rng }
+    }
+
     /// `get_random` random char
-    fn get_random(len: f32, offset: i32) -> i32 {
-        let mut rng = thread_rng();
-        let base_code: i32 = (rng.gen_range(0f32..1f32) * len).floor() as i32;
-        let char_code = base_code + offset;
-        char_code
+    fn get_random(&mut self, min: u8, max: u8) -> u8 {
+        self.rng.gen_range(min..max)
     }
 
     /// `get_random_upper` randomly generate a upper case letter
-    pub fn get_random_upper() -> String {
-        let result = Self::get_random(26f32, 65) as u8 as char;
-        String::from(result)
+    pub fn get_random_upper(&mut self) -> char {
+        self.get_random(b'A', b'Z') as char
     }
 
     /// `get_random_lower` randomly generate a lower case letters
-    pub fn get_random_lower() -> String {
-        let result = Self::get_random(26f32, 97) as u8 as char;
-        String::from(result)
+    pub fn get_random_lower(&mut self) -> char {
+        self.get_random(b'a', b'z') as char
     }
 
     /// `get_random_number` randomly generate a number
-    pub fn get_random_number() -> String {
-        let result = Self::get_random(10f32, 48) as u8 as char;
-        String::from(result)
+    pub fn get_random_number(&mut self) -> char {
+        self.get_random(b'0', b'9') as char
     }
 
     /// `get_random_symbol` randomly generate a symbol
-    pub fn get_random_symbol() -> String {
-        let symbol = "!@#$%^&*(){}[]=<>/,.";
-        let len = symbol.len() as f32;
-        let offset = 0;
-        let index = Self::get_random(len, offset) as usize;
-        let result = symbol.chars().nth(index).unwrap();
-        String::from(result)
+    pub fn get_random_symbol(&mut self) -> char {
+        let symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+        let index = self.get_random(0, symbols.len() as u8) as usize;
+        symbols.chars().nth(index).unwrap()
     }
 
     /// `get_random_bytes` randomly generate [len] bytes
-    pub fn get_random_bytes(len: usize) -> String {
+    pub fn get_random_bytes(&self, len: usize) -> String {
         let mut random_bytes_array = vec![0u8; len];
         rand::thread_rng().fill_bytes(&mut random_bytes_array);
         base64_encode_bytes(&random_bytes_array)
+    }
+
+    /// `get_random_password` randomly password
+    pub fn get_random_password(
+        &mut self,
+        has_upper: bool,
+        has_lower: bool,
+        has_number: bool,
+        has_symbol: bool,
+        length: usize,
+    ) -> String {
+        let mut char_pool = String::new();
+        let mut password: Vec<char> = Vec::with_capacity(length);
+
+        if has_upper {
+            char_pool.push_str("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            password.push(self.get_random_upper())
+        }
+        if has_lower {
+            char_pool.push_str("abcdefghijklmnopqrstuvwxyz");
+            password.push(self.get_random_lower())
+        }
+        if has_number {
+            char_pool.push_str("0123456789");
+            password.push(self.get_random_number())
+        }
+        if has_symbol {
+            char_pool.push_str("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+            password.push(self.get_random_symbol())
+        }
+
+        if char_pool.is_empty() {
+            panic!("At least one character type must be included.");
+        }
+
+        for _ in 0..(length - password.len()) {
+            password.push(*char_pool.as_bytes().choose(&mut self.rng).unwrap() as char);
+        }
+
+        password.shuffle(&mut self.rng);
+        password.iter().collect()
     }
 }
 
 #[test]
 fn password_random_test() {
-    let upper = Password::get_random_upper();
-    let lower = Password::get_random_lower();
-    let number = Password::get_random_number();
-    let symbol = Password::get_random_symbol();
-    let bytesp = Password::get_random_bytes(16);
+    let mut password = Password::new();
+    let upper = password.get_random_upper();
+    let lower = password.get_random_lower();
+    let number = password.get_random_number();
+    let symbol = password.get_random_symbol();
+    let bytesp = password.get_random_bytes(16);
+    let rpass = password.get_random_password(true, true, true, true, 16);
     println!(
-        "upper: {} lower: {} number: {} symbol: {} bytes: {}",
-        upper, lower, number, symbol, bytesp
+        "upper: {} lower: {} number: {} symbol: {} bytes: {} password: {}",
+        upper, lower, number, symbol, bytesp, rpass
     );
 }
