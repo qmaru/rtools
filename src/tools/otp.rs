187e0a8b1aba5eeea0c0f994eb68d55564200e4b
Kami Chen <csmcc2@hotmail.com>
2025-08-01 00:02:21 +0800

    eject ts

--- src/tools/otp.rs
+++ src/tools/otp.rs
@@ -1,21 +1,16 @@
 use otpauth::TOTP;
-use std::time::{SystemTime, UNIX_EPOCH};
 
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-/// `UUID` uuid tools
+/// `OPTAuth` opt auth
 pub struct OPTAuth {}
 
 #[wasm_bindgen]
 impl OPTAuth {
-    pub fn generate_code(period: u64, secret: &str) -> u32 {
+    pub fn generate_code(secret: &str, timestamp: u64, period: u64) -> u32 {
         if let Some(totp) = TOTP::from_base32(secret) {
-            let timestamp = SystemTime::now().duration_since(UNIX_EPOCH);
-            match timestamp {
-                Ok(t) => totp.generate(period, t.as_secs()),
-                Err(_) => 0,
-            }
+            totp.generate(period, timestamp)
         } else {
             0
         }
@@ -24,9 +19,15 @@ impl OPTAuth {
 
 #[test]
 fn totp_test() {
+    use std::time::{SystemTime, UNIX_EPOCH};
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
     let input = "us3whsg7x5kapv27vanwkqhf3sh3hull";
-    let uppercase = input.replace(" ", "").to_uppercase();
-    let result = OPTAuth::generate_code(30, &uppercase);
+    let secret = input.replace(" ", "").to_uppercase();
+    let result = OPTAuth::generate_code(&secret, timestamp, 30);
     println!("totp: {:?}", result);
     assert_ne!(0, result)
 }
