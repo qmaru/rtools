76400b7966b5d6615f17dcab68601111fcfd79cd
unknown <csmcc2@hotmail.com>
2025-08-11 13:06:26 +0800

    typo

--- src/tools/otp.rs
+++ src/tools/otp.rs
@@ -3,11 +3,11 @@ use otpauth::TOTP;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-/// `OPTAuth` opt auth
-pub struct OPTAuth {}
+/// `OTPAuth` otp auth
+pub struct OTPAuth {}
 
 #[wasm_bindgen]
-impl OPTAuth {
+impl OTPAuth {
     pub fn generate_code(secret: &str, timestamp: u64, period: u64) -> u32 {
         if let Some(totp) = TOTP::from_base32(secret) {
             totp.generate(period, timestamp)
@@ -27,7 +27,7 @@ fn totp_test() {
 
     let input = "us3whsg7x5kapv27vanwkqhf3sh3hull";
     let secret = input.replace(" ", "").to_uppercase();
-    let result = OPTAuth::generate_code(&secret, timestamp, 30);
+    let result = OTPAuth::generate_code(&secret, timestamp, 30);
     println!("totp: {:?}", result);
     assert_ne!(0, result)
 }
